6bec29ba3d2402180f8970b3f784a17e42364316
andrew0030 <36803991+andrew0030@users.noreply.github.com>
2025-07-06 21:52:03 +0300

    Javadocs for ManualScreenShake

--- common/src/main/java/com/github/andrew0030/pandora_core/client/screen_shaker/shakes/manual_shake/ManualScreenShake.java
+++ common/src/main/java/com/github/andrew0030/pandora_core/client/screen_shaker/shakes/manual_shake/ManualScreenShake.java
@@ -2,16 +2,28 @@ package com.github.andrew0030.pandora_core.client.screen_shaker.shakes.manual_sh
 
 import com.github.andrew0030.pandora_core.client.screen_shaker.shakes.ScreenShake;
 
+/**
+ * A manual {@link ScreenShake}, that allows for full control over the camera offsets,
+ * by continuously updating offsets using the {@code setters}.
+ * @implNote {@link ManualScreenShake} is not affected by the PaCo config settings,
+ * so constrains need to be implemented separately.
+ */
 public class ManualScreenShake extends ScreenShake {
     protected float yawDegrees = 0.0F;
     protected float pitchDegrees = 0.0F;
     protected float rollDegrees = 0.0F;
     protected boolean isFinished;
 
+    /** A new {@link ManualScreenShake} instance: */
     public ManualScreenShake() {
         super(Integer.MAX_VALUE);
     }
 
+    /**
+     * Used to flag the {@link ManualScreenShake} instance as finished,
+     * and thus should be removed from the active screen shakes.
+     * @param isFinished Whether the screen shake is finished and no longer needed.
+     */
     public void setFinished(boolean isFinished) {
         this.isFinished = isFinished;
     }
@@ -21,16 +33,32 @@ public class ManualScreenShake extends ScreenShake {
         return this.isFinished;
     }
 
+    /**
+     * Specifies an offset, that gets applied to the camera's yaw (left/right movement).
+     *
+     * @param degrees The number of degrees the camera should move in the specified direction.
+     */
     public ManualScreenShake setYaw(float degrees) {
         this.yawDegrees = degrees;
         return this;
     }
 
+
+    /**
+     * Specifies an offset, that gets applied to the camera's pitch (up/down movement).
+     *
+     * @param degrees The number of degrees the camera should move in the specified direction.
+     */
     public ManualScreenShake setPitch(float degrees) {
         this.pitchDegrees = degrees;
         return this;
     }
 
+    /**
+     * Specifies an offset, that gets applied to the camera's roll (rotational movement around the forward axis).
+     *
+     * @param degrees The number of degrees the camera should move in the specified direction.
+     */
     public ManualScreenShake setRoll(float degrees) {
         this.rollDegrees = degrees;
         return this;

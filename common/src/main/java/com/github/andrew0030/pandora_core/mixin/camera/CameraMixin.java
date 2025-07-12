37579aa6f6621ff0c3ccec4a97b12e5fd6727d2e
andrew0030 <36803991+andrew0030@users.noreply.github.com>
2025-07-12 21:57:13 +0300

    Camera related mixin cleanup
    - Removed unused fields.
    - Added note to swap zRot update from (!= zero) to delta, this would be slightly more optimal of someone modifies zRot with manual screen shake and zRot remains offset while the camera isn't moving (meaning no updates to culling are required).

--- common/src/main/java/com/github/andrew0030/pandora_core/mixin/camera/CameraMixin.java
+++ common/src/main/java/com/github/andrew0030/pandora_core/mixin/camera/CameraMixin.java
@@ -1,21 +1,14 @@
 package com.github.andrew0030.pandora_core.mixin.camera;
 
-import com.github.andrew0030.pandora_core.client.PaCoClientTicker;
 import com.github.andrew0030.pandora_core.client.screen_shaker.ScreenShakeManager;
 import com.github.andrew0030.pandora_core.mixin_interfaces.IPaCoSetCameraRotation;
 import net.minecraft.client.Camera;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.BlockPos;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
-import org.joml.Quaternionf;
-import org.joml.Vector3d;
 import org.joml.Vector3f;
-import org.lwjgl.system.MathUtil;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -27,37 +20,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Camera.class)
 public abstract class CameraMixin implements IPaCoSetCameraRotation {
     @Shadow private Vec3 position;
-
     @Shadow protected abstract void setPosition(double $$0, double $$1, double $$2);
-
     @Shadow protected abstract void move(double $$0, double $$1, double $$2);
-
-    @Shadow @Final private BlockPos.MutableBlockPos blockPosition;
-
     @Shadow public abstract void setRotation(float $$0, float $$1);
-
     @Shadow private float xRot;
     @Shadow private float yRot;
-    @Shadow @Final private Quaternionf rotation;
     @Shadow @Final private Vector3f forwards;
     @Shadow @Final private Vector3f up;
     @Shadow @Final private Vector3f left;
-
-    @Shadow protected abstract double getMaxZoom(double $$0);
-
-    @Shadow private boolean detached;
     @Shadow private BlockGetter level;
-
-    @Shadow protected abstract void setPosition(Vec3 $$0);
-
     @Shadow private Entity entity;
-    @Unique private float pandoraCore$xRot;
-    @Unique private float pandoraCore$yRot;
-    @Unique private float pandoraCore$zRot;
 
     @Unique private static final Vector3f WORLD_X = new Vector3f(1, 0, 0);
     @Unique private static final Vector3f WORLD_Y = new Vector3f(0, 1, 0);
     @Unique private static final Vector3f WORLD_Z = new Vector3f(0, 0, 1);
+    @Unique private float pandoraCore$zRot;
 
     @Inject(method = "setup", at = @At("TAIL"))
     public void cameraShaker(BlockGetter level, Entity entity, boolean detached, boolean thirdPersonReverse, float partialTick, CallbackInfo ci) {
@@ -66,33 +43,21 @@ public abstract class CameraMixin implements IPaCoSetCameraRotation {
 
     @Override
     public void pandoraCore$setRotation(float xRot, float yRot, float zRot, float partialTick) {
-//        this.pandoraCore$zRot = zRot;
-//        // This is a hack to make the game think the camera moved, as by default zRot doesn't trigger cull-frustum updates.
-//        // There is probably a more "elegant" solution by doing some mixin jank, but for the sake of simplicity this will do.
-//        if (zRot != 0.0F)
-//            xRot += partialTick * 0.000001F;
-//        this.setRotation(this.yRot + yRot, this.xRot + xRot);
+        this.pandoraCore$zRot = zRot;
+        // This is a hack to make the game think the camera moved, as by default zRot doesn't trigger cull-frustum updates.
+        // There is probably a more "elegant" solution by doing some mixin jank, but for the sake of simplicity this will do.
+        if (zRot != 0.0F) // TODO swap this for delta zRot
+            xRot += partialTick * 0.000001F;
+        this.setRotation(this.yRot + yRot, this.xRot + xRot);
 
-//        // Moves camera relatively
-//        this.move(
-//                pandoraCore$getMaxZoom(xRot, this.forwards), // X is forwards/backwards
-//                pandoraCore$getMaxZoom(yRot, this.up),       // Y is camera up/down
-//                pandoraCore$getMaxZoom(zRot, this.left)      // Z is left/right
-//        );
-
-//        // Moves camera absolutely
-//        this.setPosition(
-//                this.position.x() + xRot,
-//                this.position.y() + yRot,
-//                this.position.z() + zRot
-//        );
-
+        // Moves camera relatively
         this.move(
                 pandoraCore$getMaxZoom(xRot, this.forwards), // X is forwards/backwards
-                0,       // Y is camera up/down
-                0      // Z is left/right
+                pandoraCore$getMaxZoom(yRot, this.up),       // Y is camera up/down
+                pandoraCore$getMaxZoom(zRot, this.left)      // Z is left/right
         );
 
+        // Moves camera absolutely
         Vec3 current = this.position;
         double newX = current.x;// + pandoraCore$getMaxZoom(xRot, WORLD_X);
         double newY = current.y;// + pandoraCore$getMaxZoom(yRot, WORLD_Y);
@@ -128,16 +93,6 @@ public abstract class CameraMixin implements IPaCoSetCameraRotation {
         }
 
         return finalDistance;
-    }
-
-    @Override
-    public float pandoraCore$getXRot() {
-        return this.pandoraCore$xRot;
-    }
-
-    @Override
-    public float pandoraCore$getYRot() {
-        return pandoraCore$yRot;
     }
 
     @Override

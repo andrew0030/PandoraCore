ce722db9d545cc44023d2f077b6caac01545d173
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2025-10-23 11:58:11 -0400

    preprocess before patching

--- common/src/main/java/com/github/andrew0030/pandora_core/client/shader/templating/transformer/impl/DefaultTransformationProcessor.java
+++ common/src/main/java/com/github/andrew0030/pandora_core/client/shader/templating/transformer/impl/DefaultTransformationProcessor.java
@@ -4,13 +4,18 @@ import com.github.andrew0030.pandora_core.client.shader.templating.TemplateTrans
 import com.github.andrew0030.pandora_core.client.shader.templating.action.InsertionAction;
 import com.github.andrew0030.pandora_core.client.shader.templating.transformer.TransformationProcessor;
 import com.github.andrew0030.pandora_core.client.shader.templating.transformer.VariableMapper;
+import io.github.ocelot.glslprocessor.impl.GlslParserImpl;
 import tfc.glsl.GlslFile;
 import tfc.glsl.GlslParser;
+
+import java.util.HashMap;
 
 public class DefaultTransformationProcessor extends TransformationProcessor {
     @Override
     public String process(VariableMapper mapper, String source, TemplateTransformation transformation) {
         try {
+            source = GlslParserImpl.preprocess(source, new HashMap<>());
+
             GlslFile tree = GlslParser.parse(source);
 
             ShaderTransformer transformer = new ShaderTransformer(transformation);
